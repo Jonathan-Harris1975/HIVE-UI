6959006f4cc79ae059ac5ff6b469dc65632e9f49
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-06-20 19:56:14 +0100

    Add files via upload

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,2 +1,2 @@
-export const HIVE_UI_VERSION = '0.9.0'
-export const HIVE_UI_BUILD = 'production-ecosystem-health'
+export const HIVE_UI_VERSION = '0.9.2'
+export const HIVE_UI_BUILD = 'production-execution-gates'
