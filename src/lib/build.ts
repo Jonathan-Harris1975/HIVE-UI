bb25cf327ad3537d066be4f22153211d6bb15896
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-06-20 23:07:47 +0100

    Add files via upload

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,2 +1,2 @@
-export const HIVE_UI_VERSION = '0.10.3'
-export const HIVE_UI_BUILD = 'file-skill-apply-flow'
+export const HIVE_UI_VERSION = '0.10.4'
+export const HIVE_UI_BUILD = 'skill-catalogue-cleanup'
