55972b7c6e58376d3750cade14c894ef482dbfe8
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-06-20 21:28:41 +0100

    Add files via upload

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,2 +1,2 @@
-export const HIVE_UI_VERSION = '0.10.0'
-export const HIVE_UI_BUILD = 'r2-write-skill-models'
+export const HIVE_UI_VERSION = '0.10.1'
+export const HIVE_UI_BUILD = 'mobile-contrast-pass'
