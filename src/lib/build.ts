ea1749719e012b04cddc6de92e3831cd47d2a0d2
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-06-22 15:29:50 +0100

    Add files via upload

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,2 +1,2 @@
-export const HIVE_UI_VERSION = '0.10.7'
-export const HIVE_UI_BUILD = 'version-readiness-sync'
+export const HIVE_UI_VERSION = '0.10.8'
+export const HIVE_UI_BUILD = 'review-state-sync'
