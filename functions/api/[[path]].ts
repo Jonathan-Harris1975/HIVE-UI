cb454f80ce2ae9a06c8358618953ddd882287247
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-07-05 20:27:20 +0100

    Add files via upload

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -29,7 +29,7 @@ interface LoginAttempt {
   resetAt: number
 }
 
-const UI_VERSION = '0.10.11'
+const UI_VERSION = '0.11.0'
 const LOGIN_WINDOW_MS = 10 * 60 * 1000
 const LOGIN_MAX_FAILURES = 5
 const loginAttempts = new Map<string, LoginAttempt>()
