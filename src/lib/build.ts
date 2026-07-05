65a1ddf4504f9984e05117bffb73c79ff4fb9bfd
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-07-05 15:59:15 +0100

    Add files via upload

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,2 +1,2 @@
-export const HIVE_UI_VERSION = '0.10.10'
-export const HIVE_UI_BUILD = 'catalogue-metadata'
+export const HIVE_UI_VERSION = '0.10.11'
+export const HIVE_UI_BUILD = 'a11y-hardening-pass'
