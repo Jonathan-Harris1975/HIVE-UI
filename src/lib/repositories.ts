9545e09452557ba5f39147b09bb7145833c160d0
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-08-26 16:49:19 +0100

    Add files via upload

---
+++ src/lib/repositories.ts
@@ -0,0 +1,12 @@
+export const GOVERNED_REPOSITORIES: readonly string[] = [
+  'HIVE',
+  'HIVE-UI',
+  'AIMS',
+  'AIMS-UI',
+  'RAMS',
+  'MAST',
+  'IRS',
+  'Website',
+]
+
+export const MEMORY_REPOSITORIES: readonly string[] = [...GOVERNED_REPOSITORIES, 'Shared']
